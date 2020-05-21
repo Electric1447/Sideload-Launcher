52d3a5de6d053534694628fcdb904781df4667ee
Electric <itai.levin4@gmail.com>
2020-05-21 15:42:46 +0300

    Hotfix (v1.0.1)

--- app/src/main/java/eparon/sideloadlauncher/MainActivity.java
+++ app/src/main/java/eparon/sideloadlauncher/MainActivity.java
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.TextView;
@@ -73,8 +72,6 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     private void setFullScreen () {
         try {
-            View decorView = getWindow().getDecorView();
-
             int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -82,9 +79,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                     | View.SYSTEM_UI_FLAG_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_IMMERSIVE;
 
-            decorView.setSystemUiVisibility(uiOptions);
-
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER, WindowManager.LayoutParams.FLAG_SHOW_WALLPAPER);
+            getWindow().getDecorView().setSystemUiVisibility(uiOptions);
 
         } catch (Exception e) {
             e.printStackTrace();
